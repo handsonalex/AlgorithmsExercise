1bbea72fc262ff19994d7abdf99c524d10afd8ab
Joseph_Ho <378274190@qq.com>
2021-03-08 21:38:01 +0800

    替换空格

---
+++ src/getOffer/ReplaceSpace.java
@@ -0,0 +1,12 @@
+package getOffer;
+//请实现一个函数，把字符串 s 中的每个空格替换成"%20"
+
+
+public class ReplaceSpace {
+    //也可以用StringBuilder
+
+    public  String mySolution(String s) {
+        return s.replace(" " ,"%20");
+    }
+
+}
